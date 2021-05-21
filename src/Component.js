57913d3b9ec116795dd9e75cbb53c86613c18907
itsamelaurio <mslaurabarba@gmail.com>
2021-05-21 12:58:07 +0200

    Co-authored-by: rebeckapersson <rebecka_fotboll95@hotmail.com> Co-authored-by: hannils98 <62596360+hannils98@users.noreply.github.com>

--- src/Component.js
+++ src/Component.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
 export default function Component() {
+  let [image, setImage] = React.useState("");
 
-    let [image, setImage] = React.useState("");
+  function componentDidMount() {
+    axios
+      .get(`https://api.thedogapi.com/v1/images/search`, {
+        headers: {
+          "x-api-key": "30777f0e-7f95-4c79-b4b3-b657b6bdd296",
+        },
+      })
+      .then((res) => {
+        const data = res.data;
+        console.log(data[0]["url"]);
+        setImage(data[0]["url"]);
+      });
+  }
 
-    function componentDidMount() {
-        axios.get(`https://api.thedogapi.com/v1/images/search`,
-        {
-            headers:{
-                "x-api-key": "30777f0e-7f95-4c79-b4b3-b657b6bdd296"
-            }
-        })
-          .then(res => {
-            const data = res.data;
-            console.log(data[0]["url"]);
-            setImage(data[0]["url"]);
-          })
-
-      }
-
-    return (
-        <div>
-            <h1>HelloHello</h1>
-            <img src={image}/>
-            <button  className = "btn btn-danger" onClick={() => componentDidMount()}> Click me</button>
-
-        </div>
-    )
+  return (
+    <div>
+      <h1>woofwoof</h1>
+      <img src={image} />
+      <button className="btn btn-danger" onClick={() => componentDidMount()}>
+        {" "}
+        Click me
+      </button>
+    </div>
+  );
 }
