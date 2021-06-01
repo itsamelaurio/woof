2757b7687a096ef219ed28c035a1632c854c0de1
itsamelaurio <mslaurabarba@gmail.com>
2021-06-01 14:45:18 +0200

    no need to press Navbar to populate dropdown anymore

--- src/Favorites.js
+++ src/Favorites.js
@@ -18,7 +18,7 @@ export default function Favorites(props) {
         <div> 
             <div id="favorites">
                 <button className="btn btn-danger" onClick={() => sortBreed()}> Sort by breed </button>
-                <button className="btn btn-danger" onClick={() => sortDate()}> Sortby date </button>
+                <button className="btn btn-danger" onClick={() => sortDate()}> Sort by date </button>
                 <div id="dog-list">
                 {props.favorites.map(dog => <div className="favorite" key={dog.date}> <h6>{dog.breed}</h6> <img src="images/delete.png" alt="delete dog" className="delete-dog" onClick={() => deleteFavorite(dog.id, props.favorites)}></img> <img src={dog.url} id={dog.id} /> </div>)}
                 </div>
